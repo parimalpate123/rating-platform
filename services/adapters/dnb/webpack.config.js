b99290233a9b45d16a75ff64911d338c1a22e73e
parimalpate123 <parimalpatel123@gmail.com>
2026-02-22 15:49:00 -0500

    fixing deploy erros

--- services/adapters/dnb/webpack.config.js
+++ services/adapters/dnb/webpack.config.js
@@ -2,5 +2,5 @@ const { NxAppWebpackPlugin } = require("@nx/webpack/app-plugin");
 const { join } = require("path");
 module.exports = {
   output: { path: join(__dirname, "dist"), clean: true },
-  plugins: [new NxAppWebpackPlugin({ target: "node", compiler: "tsc", main: "./src/main.ts", tsConfig: "./tsconfig.app.json", assets: [], optimization: false, outputHashing: "none", generatePackageJson: false, sourceMap: true })],
+  plugins: [new NxAppWebpackPlugin({ target: "node", compiler: "tsc", main: "./src/main.ts", tsConfig: "./tsconfig.app.json", assets: [], optimization: false, outputHashing: "none", generatePackageJson: true, sourceMap: true })],
 };
